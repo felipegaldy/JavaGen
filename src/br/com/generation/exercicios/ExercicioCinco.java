8e6f08ac04457f7c65d51fcdace638e82f5b4913
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-06-06 17:38:00 -0300

    resolucao ex005

---
+++ src/br/com/generation/exercicios/ExercicioCinco.java
@@ -0,0 +1,29 @@
+package br.com.generation.exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioCinco {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 5. Faça um sistema que leia as 3 notas de um aluno e calcule a média final deste
+			aluno. Considerar que a média é ponderada e que o peso das notas é: 2,3 e 5,
+			respectivamente.
+		 */
+		
+		double nota1, nota2, nota3, media;
+		Scanner leia = new Scanner(System.in);
+		System.out.println("Digite o valor da nota 1:");
+		nota1 = leia.nextDouble();
+		System.out.println("Digite o valor da nota 2:");
+		nota2 = leia.nextDouble();
+		System.out.println("Digite o valor da nota 3:");
+		nota3 = leia.nextDouble();
+		media = (nota1*2 + nota2*3 + nota3*5) /10;
+		System.out.println(media);
+		
+
+	}
+
+}
