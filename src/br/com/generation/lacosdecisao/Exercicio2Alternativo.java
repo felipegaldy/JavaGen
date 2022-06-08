3538f5820caf3cb97bb54ddc7cdcafaae461799b
felipegaldy <felipe.silveira4@fatec.sp.gov.br>
2022-06-08 11:09:24 -0300

    resolucao do ex01 for

---
+++ src/br/com/generation/lacosdecisao/Exercicio2Alternativo.java
@@ -0,0 +1,20 @@
+package br.com.generation.lacosdecisao;
+
+import java.util.Iterator;
+
+public class Exercicio2Alternativo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Faça um programa que entre com três números e coloque em ordem crescente.
+		 */
+		
+		double[] numeros = new double[3];	
+		for(int i=0;i < 3; i++) {
+			
+		}
+
+	}
+
+}
